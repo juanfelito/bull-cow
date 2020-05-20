fc3bbfc65ac2af30c09b6bd82ce772e03b7cb835
Felipe Balcero <felipe.balcero45@gmail.com>
2020-05-20 12:01:49 -0500

    Checkpoint

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,6 +4,7 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine("Welcome to the Bull Cow Game");
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
